c3fe53195a70b79e518a35ca0d9a621bd036ca3b
sravya-eluri12 <sravyaeluri12@gmail.com>
2024-09-22 13:32:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const secondHand = document.querySelector('.second-hand');
+    const minsHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    function setDate() {
+      const now = new Date();
+      const utcOffset = 5.5 * 60 * 60 * 1000; // UTC+5:30 in milliseconds
+      const istTime = new Date(now.getTime() + utcOffset);
+
+      const seconds = istTime.getSeconds();
+      const secondsDegrees = ((seconds / 60) * 360) + 90;
+      secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+      const mins = istTime.getMinutes();
+      const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+      minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+      const hour = istTime.getHours();
+      const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+      hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+    }
+
+    setInterval(setDate, 1000);
+    setDate();
